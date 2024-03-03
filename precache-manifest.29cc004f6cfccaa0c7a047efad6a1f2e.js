a523ea57eafc765982d4502345036ad604c3400f
Jayesh Sawant <34836779+j23saw@users.noreply.github.com>
2024-03-03 08:23:39 +0000

    Updates

---
+++ precache-manifest.29cc004f6cfccaa0c7a047efad6a1f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1544c83fbf76e9101a3c9bacc0072a",
+    "url": "/covidsim/index.html"
+  },
+  {
+    "revision": "2e6c78ad3d04ed66e2a4",
+    "url": "/covidsim/static/css/main.0a7f6686.chunk.css"
+  },
+  {
+    "revision": "75aed59330803159d29d",
+    "url": "/covidsim/static/js/2.864ce3d7.chunk.js"
+  },
+  {
+    "revision": "6976d593e6af9fd61d4e7fb6896125bf",
+    "url": "/covidsim/static/js/2.864ce3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6c78ad3d04ed66e2a4",
+    "url": "/covidsim/static/js/main.8105e0fe.chunk.js"
+  },
+  {
+    "revision": "5adbd080b2f188d63cd1",
+    "url": "/covidsim/static/js/runtime-main.db397632.js"
+  }
+]);
